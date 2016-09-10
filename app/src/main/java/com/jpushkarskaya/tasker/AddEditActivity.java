59d022db8db61f2230f0f09231cff50a7757a018
jpushkarskaya <jpushkarskaya@gmail.com>
2016-09-10 14:46:05 -0700

    AddEdit page; add, delete photos

---
+++ app/src/main/java/com/jpushkarskaya/tasker/AddEditActivity.java
@@ -0,0 +1,20 @@
+package com.jpushkarskaya.tasker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class AddEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit);
+    }
+
+    public void onSave(View view) {
+    }
+
+    public void onDelete(View view) {
+    }
+}
